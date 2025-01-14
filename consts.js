95d2905a550f5f922ffae327756161ca2b7373e2
fatimeelizade <fatimelizade@gmail.com>
2025-01-14 18:12:57 +0400

    first commit

---
+++ consts.js
@@ -0,0 +1,7 @@
+export const input = document.querySelector("input");
+export const buttonAdd = document.querySelector(".textt button");
+export const box = document.querySelector(".box");
+export const deletee = document.querySelector(".delete");
+export const btnYes =document.querySelector(".button-yes");
+export const btnNo =document.querySelector(".button-no");
+export const ol = document.createElement("ol");
--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const input = document.querySelector("input");
-const buttonAdd = document.querySelector(".textt button");
-const box = document.querySelector(".box");
-const deletee = document.querySelector(".delete");
-const btnYes =document.querySelector(".button-yes");
-const btnNo =document.querySelector(".button-no");
-const ol = document.createElement("ol");
+import {input,buttonAdd,box,deletee,btnYes,btnNo,ol} from './consts.js';
 ol.style.listStyleType="none";
 ol.style.marginTop="30px";
 buttonAdd.addEventListener("click", function() {
@@ -20,26 +14,33 @@ buttonAdd.addEventListener("click", function() {
     buttonRemove.textContent="Remove";
     buttonRemove.classList.add("button-remove");
     h2.textContent = input.value;
+    localStorage.setItem
     li.append(actions);
     actions.append(buttonEdit);
     actions.append(buttonRemove);
     li.append(h2);
     ol.append(li);
     box.append(ol);
-    buttonEdit.addEventListener("click",function(){
-        h2.textContent = input.value;
-        input.value = "";
+    buttonEdit.addEventListener("click", function() {
+        if (buttonEdit.textContent === "Edit") {
+            buttonEdit.textContent = "Save";
+            input.value = h2.textContent;
+        } 
+        else {
+            buttonEdit.textContent = "Edit";
+            h2.textContent = input.value;
+            input.value = "";
+        }
     });
-    buttonRemove.addEventListener("click",function(){
-        deletee.classList.remove("d-none")
-        btnYes.addEventListener("click",function(){
-            h2.textContent="";
-            actions.remove();
+    buttonRemove.addEventListener("click", function() {
+        deletee.classList.remove("d-none");
+        btnYes.addEventListener("click", function() {
+            li.remove();
             deletee.classList.add("d-none");
-        });
-        btnNo.addEventListener("click",function(){
+           })
+        btnNo.addEventListener("click", function() {
             deletee.classList.add("d-none");
-        });
+        })
     });
     input.value = "";
 });
